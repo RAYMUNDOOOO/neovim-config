22f04eed6cf50ab76bb5b6cd36df62384ca047cb
RAYMUNDOOOO <rtovar.develops@gmail.com>
2023-07-16 23:06:41 +1000

    configured lsp-zero

---
+++ lua/ramon/lsp/server-config.lua
@@ -0,0 +1,21 @@
+-- Installing new language servers
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+	local opts = {buffer = bufnr}
+
+	-- Format the current buffer with 'gq'
+	vim.keymap.set({'n', 'x'}, 'gq', function()
+		vim.lsp.buf.format({async = false, timeout_ms = 10000})
+	end, opts)
+end)
+
+lsp.ensure_installed({
+	'clangd',
+	'lua_ls',
+	'marksman',
+	'powershell_es',
+})
+
+lsp.setup()
